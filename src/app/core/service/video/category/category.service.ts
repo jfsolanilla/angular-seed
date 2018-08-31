b2db1a586ed992b966c060e0b5e5881315320774
Julian Franco <julian.franco@thinkwrap.com>
2018-08-31 14:39:32 -0500

    video-dashboard. Getting videos by a category

--- src/app/core/service/video/category/category.service.ts
+++ src/app/core/service/video/category/category.service.ts
@@ -11,10 +11,13 @@ export class CategoryService {
 
   getAll() {
     // return this.http.get('https://api.vimeo.com/categories?direction=asc&access_token=204359e5c2d7ce2911e1d51741a740f2');
-    return this.http.get<Category>('https://api.vimeo.com/categories?direction=asc&access_token=6cd2ed3009596ecd5774129978575c49');
+    return this.http.get<Category>('https://api.vimeo.com/categories?access_token=6cd2ed3009596ecd5774129978575c49');
   }
 
   getByCategory(category) {
-    return this.http.get<Category>(`${category.link}&access_token=${category.resource_key}`);
+    return this.http.get<Category>(`https://api.vimeo.com${category.uri}/videos?access_token=6cd2ed3009596ecd5774129978575c49`);
   }
 }
+
+//Loader for category queries
+//Toastr when user exists, register at least one user, user doesn't exist
